0dac7f0bf860deae3cce2a48ba299a5d36b6c0a0
Daria <dashusik_@mail.ru>
2024-07-01 17:17:51 +0300

    Добавялет скрипты для сборки

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser-js';
+import squoosh from 'gulp-libsquoosh';
+
 
 // Styles
 
@@ -16,6 +20,41 @@ export const styles = () => {
     ]))
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+//HTML
+const html = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest('build'));
+}
+
+//scripts
+ const scripts = () => {
+  return gulp.src('source/js/*.js')
+  .pipe(terser())
+  .pipe(gulp.dest('build/js'));
+}
+
+//Images
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/img'));
+}
+
+ const copyImages = () => {
+    return gulp.src('source/img/**/*. {jpg,png}')
+    .pipe(gulp.dest('build/img'));
+}
+
+//WebP
+ const createWebP = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(squoosh({
+    webp: {}
+  }))
+  .pipe(gulp.dest('build/img'));
 }
 
 // Server
